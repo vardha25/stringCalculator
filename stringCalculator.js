4d4faf1aada79044e4e33a2afb21c4d5c91440ef
Vardha Jain <vardhajain25@gmail.com>
2024-09-27 15:17:19 +0530

    handling of commas and return sum of comma separated numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,11 @@
 function add(numbers) {
     if (numbers === '') return 0;
-    return +numbers;
+    const simplifiedNumbers = numbers.replace(/,/g , "").split('');
+    const sum = simplifiedNumbers.reduce((total, num) => {
+        const n = parseInt(num, 10);
+        return total + n;
+      }, 0);
+    return sum;
 }
 
 module.exports = { add };
