576fc271fa0e087d6d74156524779cede256e580
Vardha Jain <vardhajain25@gmail.com>
2024-09-27 15:33:16 +0530

    Extracting and replacing delimiter with empty space from numbers string

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,11 +1,22 @@
 function add(numbers) {
     if (numbers === '') return 0;
-    const simplifiedNumbers = numbers.replace(/,/g , "").split('');
+    const delimiter = findDelimiter(numbers);
+    const simplifiedNumbers = numbers.replace(/\/\/.*\n/, '').split(new RegExp(`[${delimiter}\n]`));
     const sum = simplifiedNumbers.reduce((total, num) => {
         const n = parseInt(num, 10);
         return total + n;
       }, 0);
     return sum;
 }
+
+//Extracting delimiter from string of format "//[delimiter]\n[numbers…]".
+function findDelimiter(numbers) {
+    const defaultDelimiter = ',';
+    if (numbers.startsWith('//')) {
+      const customDelimiter = numbers.split('\n')[0].slice(2);
+      return customDelimiter;
+    }
+    return defaultDelimiter;
+  }
 
 module.exports = { add };
